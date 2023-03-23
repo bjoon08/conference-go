81656f0df6aa29330ed5828d909447e6d3d4ef4d
Joon Hyuk Brandon Jang <joonbjang@gmail.com>
2023-03-22 19:48:16 -0700

    Restfulize part 2 is done.

--- presentations/api_views.py
+++ presentations/api_views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
-from .models import Presentation
+from .models import Presentation, Status
 from common.json import ModelEncoder
+from django.views.decorators.http import require_http_methods
+import json
 
 
 class PresentationListEncoder(ModelEncoder):
@@ -26,6 +28,7 @@ class PresentationDetailEncoder(ModelEncoder):
     ]
 
 
+@require_http_methods(["GET", "POST"])
 def api_list_presentations(request, conference_id):
     """
     Lists the presentation titles and the link to the
@@ -48,13 +51,36 @@ def api_list_presentations(request, conference_id):
         ]
     }
     """
-    presentations = Presentation.objects.filter(conference=conference_id)
-    return JsonResponse(
-        {"presentations": presentations},
-        encoder=PresentationListEncoder,
-    )
+    if request.method == "GET":
+        presentations = Presentation.objects.filter(conference=conference_id)
+        return JsonResponse(
+            {"presentations": presentations},
+            encoder=PresentationListEncoder,
+        )
+    elif request.method == "POST":
+        try:
+            content = json.loads(request.body)
+            status = Status.objects.get(id=content["status"])
+            content["status"] = status
+            presentation = Presentation.objects.create(**content)
+            return JsonResponse(
+                presentation,
+                encoder=PresentationDetailEncoder,
+                safe=False,
+            )
+        except KeyError:
+            return JsonResponse(
+                {"message": "status is missing in the request body"},
+                status=400,
+            )
+        except Status.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid status"},
+                status=400,
+            )
 
 
+@require_http_methods(["DELETE", "GET", "PUT"])
 def api_show_presentation(request, id):
     """
     Returns the details for the Presentation model specified
@@ -80,9 +106,32 @@ def api_show_presentation(request, id):
         }
     }
     """
-    presentation = Presentation.objects.get(id=id)
-    return JsonResponse(
-        presentation,
-        encoder=PresentationDetailEncoder,
-        safe=False,
-    )
+    if request.method == "GET":
+        presentation = Presentation.objects.get(id=id)
+        return JsonResponse(
+            presentation,
+            encoder=PresentationDetailEncoder,
+            safe=False,
+        )
+    elif request.method == "DELETE":
+        count, _ =Presentation.objects.filter(id=id).delete()
+        return JsonResponse({"deleted": count > 0})
+    else:
+        content = json.loads(request.body)
+        try:
+            if "status" in content:
+                status = Status.objects.get(id=content["status"])
+                content["status"] = status
+        except Status.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid status"},
+                status=400,
+            )
+
+        Presentation.objects.filter(id=id).update(**content)
+        presentation = Presentation.objects.get(id=id)
+        return JsonResponse(
+            presentation,
+            encoder=PresentationDetailEncoder,
+            safe=False,
+        )
